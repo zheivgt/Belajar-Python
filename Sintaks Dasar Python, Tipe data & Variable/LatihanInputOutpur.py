21d3af0483e57299ab57640cb0e1af82a0035ec7
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-12-09 18:24:35 +0700

    Belajar Sintaks Dasar Python, Tipe data & Variable

---
+++ Sintaks Dasar Python, Tipe data & Variable/LatihanInputOutpur.py
@@ -0,0 +1,14 @@
+print ("     Data Diri Mahasiswa     ")
+print ("-----------------------------")
+
+nim = input ("Masukan NIM anda : ")
+nama = input ("Masukan Nama anda : ")
+jurusan = input ("Jurusan : ")
+alamat = input ("Alamat : ")
+
+print ("-----------------------------")
+print ("Hasil Cetak data diatas :")
+print ("NIM : " +str(nim))
+print ("Nama : " +str(nama))
+print ("Jurusan : " +str(jurusan))
+print ("Alamat : " +str(alamat))
